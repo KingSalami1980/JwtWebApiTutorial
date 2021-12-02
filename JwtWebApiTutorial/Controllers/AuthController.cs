6bf32e324befb36b6a52191818c858c72ecfac53
Clive Harding <clive.harding@daimler.com>
2021-12-02 07:10:36 +0200

    Some clean up.

--- JwtWebApiTutorial/Controllers/AuthController.cs
+++ JwtWebApiTutorial/Controllers/AuthController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
